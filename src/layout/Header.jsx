eca42296b818700eef9ca6987a08321ac3d5c775
Surachotkit <surachot.kit@gmail.com>
2023-10-07 02:15:09 +0700

    add header

---
+++ src/layout/Header.jsx
@@ -0,0 +1,24 @@
+import logo from "../img/logo.png";
+
+export default function Header() {
+  return (
+    <>
+      <div className="flex items-center gap-8 p-6 ">
+        <div className="flex items-center gap-4 cursor-pointer">
+          <img src={logo} alt="logo" />
+          <span className=" text-[24px] font-semibold text-red-500 ">BINNAKUB</span>
+        </div>
+        <div className=" text-gray-500 hover:text-red-500 cursor-pointer">Markets</div>
+
+        <div className="flex gap-4">
+          <button className="bg-gray-100 px-6 rounded-full text-gray-500 hover:bg-gray-200">
+            Login
+          </button>
+          <button className="bg-red-500 px-6 text-white p-2 rounded-full hover:bg-red-600">
+            Register
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}

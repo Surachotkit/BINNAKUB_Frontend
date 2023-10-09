f5558355b0de37147d3f07a2729162ef61a5f0ad
Surachotkit <surachot.kit@gmail.com>
2023-10-10 00:23:54 +0700

    add Login register

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,9 +6,14 @@ export default function Header() {
       <div className="flex items-center gap-8 p-6 ">
         <div className="flex items-center gap-4 cursor-pointer">
           <img src={logo} alt="logo" />
-          <span className=" text-[24px] font-semibold text-red-500 ">BINNAKUB</span>
+          <span className=" text-[24px] font-semibold text-red-500 ">
+            BINNAKUB
+          </span>
         </div>
-        <div className=" text-gray-500 hover:text-red-500 cursor-pointer">Markets</div>
+
+        <div className=" text-gray-500 hover:text-red-500 cursor-pointer">
+          Markets
+        </div>
 
         <div className="flex gap-4">
           <button className="bg-gray-100 px-6 rounded-full text-gray-500 hover:bg-gray-200">

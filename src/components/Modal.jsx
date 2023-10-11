e7e0cc4bac9495c08b224c63ee947dabbfd1380f
Surachotkit <surachot.kit@gmail.com>
2023-10-12 01:25:00 +0700

    update state close modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import usd from "../img/icon/usd.png";
 import usdt from "../img/icon/usdt.png";
-export default function Modal() {
+
+
+export default function Modal({onClose}) {
+   
   return (
     <>
       {/* black dog */}
@@ -10,12 +14,15 @@ export default function Modal() {
           {/* container */}
           <div className="flex flex-col justify-center rounded-lg w-[30rem] h-full py-6 bg-white shadow-2xl border">
             <div className="flex justify-between p-4 text-xl">
+
               <div className="flex items-center gap-2">
                 <img src={usd} alt="usd" />
                 <span className="font-bold">US Dollar</span>
                 <span className="flex text-sm">(USD)</span>
               </div>
+
             </div>
+            
             <span className="flex px-6 pb-4">$ 1</span>
 
             <div className="flex flex-col items-center gap-5">
@@ -49,7 +56,7 @@ export default function Modal() {
                 <button className="bg-red-500 w-full rounded-md hover:bg-red-600 text-white h-full p-2">
                   Deposit
                 </button>
-                <button className="bg-gray-200 w-full rounded-md hover:bg-gray-300 text-[#6D6D6D] h-full p-2">
+                <button className="bg-gray-200 w-full rounded-md hover:bg-gray-300 text-[#6D6D6D] h-full p-2" onClick={onClose} >
                   Cancel
                 </button>
               </div>
--- src/features/market/MarketForm.jsx
+++ src/features/market/MarketForm.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Modal from "../../components/Modal";
-import MarketButton from "./MarketButton";
 import MarketContent from "./MarketContent";
 import MarketItem from "./MarketItem";
 import MarketMenu from "./MarketMenu";
 
 export default function MarketForm() {
+  const [isOpen, setIsOpen] = useState(true);
+  console.log(isOpen)
+
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div className="flex flex-col gap-4">
-      <Modal/>
+      {isOpen && (<Modal open={isOpen} onClose={toggleModal} />)}
       <div className="flex justify-center items-center">
         <MarketContent />
       </div>
